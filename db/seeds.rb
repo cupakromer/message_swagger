f2260e86d6dc32416321e969017f468e6ae60bbc
Aaron Kromer <cupakromer@users.noreply.github.com>
2022-03-11 10:46:12 -0500

    Add some seeds to ease local dev

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+author = User.create!(handle: "the-real-author", name: "Any Author")
+friend = User.create!(handle: "your-friend", name: "Any Friend")
+_no_dms = User.create!(handle: "no-sliding-into-my-dms!", name: "No DMs")
+
+dm_author = author.create_direct_depot!
+dm_friend = friend.create_direct_depot!
+
+author.authored_messages.create!(depot: dm_author, content: "Is this thing on?")
+author.authored_messages.create!(depot: dm_friend, content: "Hello? Can you hear me?")
+friend.authored_messages.create!(depot: dm_author, content: "Can you hear me?")
