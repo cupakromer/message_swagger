c663cc6be87bfb42822f7c984dbed28960b3aa45
Aaron Kromer <cupakromer@users.noreply.github.com>
2022-03-11 01:36:30 -0500

    Add basic depot model
    This creates the `depots` table per ADR 20220310190424.
    
    While not called out explicitly in the text of the ADR, the association
    between a `Depot` and other resources is one-to-one per the schema
    diagram. Thus, to preserve the data integrity of those associations we
    make the index for the `receiver` reference unique. That ensures we only
    ever have one such relation in the database.

---
+++ app/models/depot.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# ## Schema Information
+#
+# Table name: `depots`
+#
+# ### Columns
+#
+# Name                 | Type               | Attributes
+# -------------------- | ------------------ | ---------------------------
+# **`id`**             | `bigint`           | `not null, primary key`
+# **`receiver_type`**  | `string`           | `not null`
+# **`created_at`**     | `datetime`         | `not null`
+# **`updated_at`**     | `datetime`         | `not null`
+# **`receiver_id`**    | `bigint`           | `not null`
+#
+# ### Indexes
+#
+# * `index_depots_on_receiver` (_unique_):
+#     * **`receiver_type`**
+#     * **`receiver_id`**
+#
+class Depot < ApplicationRecord
+  belongs_to :receiver, polymorphic: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_10_180742) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_11_061736) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "depots", force: :cascade do |t|
+    t.string "receiver_type", null: false
+    t.bigint "receiver_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["receiver_type", "receiver_id"], name: "index_depots_on_receiver", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "handle", limit: 120, null: false
